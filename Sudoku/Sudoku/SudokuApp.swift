277e4bf5535de8c4e4bb8bc4eeda16997bdd18ca
Ghislain Leblanc <ghislain.leblanc@clevertech.biz>
2020-12-27 14:07:55 -0500

    SwiftUI lifecycle.

---
+++ Sudoku/Sudoku/SudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuApp.swift
+//  Sudoku
+//
+//  Created by Ghislain Leblanc on 2020-12-27.
+//
+
+import SwiftUI
+
+@main
+struct SudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
