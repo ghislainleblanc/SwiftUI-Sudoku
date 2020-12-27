567ac23f49115250b5879efa91df864b307ec165
Ghislain Leblanc <ghislain.leblanc@clevertech.biz>
2020-12-27 14:51:45 -0500

    Basic layout,

--- Sudoku/Sudoku/ContentView.swift
+++ Sudoku/Sudoku/ContentView.swift
@@ -9,13 +9,336 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            VStack {
+                VStack {
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                }
+                VStack {
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                }
+                VStack {
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                }
+            }
+            VStack {
+                VStack {
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                }
+                VStack {
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                }
+                VStack {
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                }
+            }
+            VStack {
+                VStack {
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                }
+                VStack {
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                }
+                VStack {
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                    HStack {
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                        Text("?")
+                            .foregroundColor(Color.green)
+                            .padding()
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
